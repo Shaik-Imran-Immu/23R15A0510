377b7488d75592ab4c8e61256d049f077e419696
Imran Shaik <shaik.imranimmu66@gmail.com>
2025-08-02 12:45:25 +0530

    Delete src directory

--- src/routes/shorturls.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createShortUrl, redirectShortUrl, getStats } = require('../controllers/urlController');
-
-router.post('/shorturls', createShortUrl);
-router.get('/shorturls/:code', getStats);
-router.get('/:code', redirectShortUrl);
-
-module.exports = router;
--- src/utils/generateShortcode.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-function generateShortcode(length = 6) {
-  return crypto.randomBytes(length).toString('base64url').slice(0, length);
-}
-
-module.exports = generateShortcode;
